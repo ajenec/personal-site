5e559087f51a380c527009b5ef872962f5a45e48
Ajene <ajenec@gmail.com>
2025-09-30 13:34:17 -0400

    installed tailwind

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {}
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <h1 className="text-3xl font-bold text-gray-900">Tailwind is working</h1>
+    </div>
+  );
+}
 
 export default App;
